76841649d2d85b804747d5e324a64c70b4e4327e
Felipe Maiocch <maiocch@MacBook-Pro-de-Felipe.local>
2025-02-13 23:25:54 -0300

    Ajustando configurações do Gunicorn e dependências para melhorar estabilidade

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,9 @@
 bind = "0.0.0.0:10000"
 workers = 1
 threads = 2
-timeout = 300  # 5 minutos
-worker_class = "gthread" 
+timeout = 120
+worker_class = "sync"
+preload_app = True
+max_requests = 1000
+max_requests_jitter = 50
+keepalive = 5 
